2f309bea25261ce921d19987a0d7901ebc7acbd5
Boro Nikic <boro.nikic@sony.com>
2024-03-27 11:10:14 +0100

    1. Added progress 2. Added support for build and run using maven

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # 1337-assignment
-Job interview assignment
+
+Design decisions:
+1. Divide into logically independent units that can be tested individually
+2. Recursion and parallel execution to achieve reasonable performance
+3. Use of existing open source libraries, which can shorten development time
+
+Tools:
+1. java 17
+2. maven (tested with version 3.9.4)
+3. IDE (Optional, I used Eclipse)
+
+Here's how to compile and run:
+1. Clone the allocation repository to your local computer
+2. Open terminal in the folder with pom.xml in it
+3. In the terminal window run: [mvn clean compile exec:java]
+      - maven will compile and run the program
+      - at the top of the terminal window you will find a line OUTPUT:
--- assignment/src/main/java/boro/assignment/Program.java
+++ assignment/src/main/java/boro/assignment/Program.java
@@ -18,29 +18,18 @@ public class Program
 {
 	public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException
 	{
-		long start = System.currentTimeMillis();
-
 		URI uri = new URI("https://books.toscrape.com/index.html");
 		Path destination = Path.of(System.getProperty("java.io.tmpdir")).resolve("assignment-1337").resolve(UUID.randomUUID().toString());
 
-		Files.createDirectories(destination);
+		System.out.println(format("OUTPUT: %s", destination));
+		System.out.print("  Traversing...");
 
-		System.out.println(format("Traversing uri: %s", uri));
+		Files.createDirectories(destination);
 
 		ThreadPoolExecutor executorService = new ThreadPoolExecutor(100, 100, 10L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
 		executorService.allowCoreThreadTimeOut(true); //Enable program to exit
 
 		LinkTraverser traverser = new LinkTraverser(executorService);
 		traverser.traverse(uri, destination);
-
-		Runtime.getRuntime().addShutdownHook(new Thread(){
-			@Override
-			public void run()
-			{
-				long end = System.currentTimeMillis();
-				long seconds = ((end -start)/1000)%60;
-				System.out.println(format("\nDONE in [%d sec] - Output: %s", seconds, destination));
-			}
-		});
 	}
 }
